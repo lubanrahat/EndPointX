66c39314dada62332427bd15dbe00380bb9ecc13
lubanrahat <lubanrahat.dev@gmail.com>
2026-02-02 17:11:12 +0600

    standardize JSX formatting in SignInPage component

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -7,33 +7,47 @@ import Link from "next/link";
 
 export default function SignInPage() {
   return (
-     <section className='flex min-h-screen bg-zinc-50 dark:bg-transparent px-4 py-16 md:py-32 '>
-      <div className='bg-card m-auto h-fit w-full max-w-sm rounded-[calc(var(--radius)+.125rem)] border p-0.5 shadow-md dark:[--color-muted:var(--color-zinc-900)] '>
-        <div className='p-8 pb-6'>
+    <section className="flex min-h-screen bg-zinc-50 dark:bg-transparent px-4 py-16 md:py-32 ">
+      <div className="bg-card m-auto h-fit w-full max-w-sm rounded-[calc(var(--radius)+.125rem)] border p-0.5 shadow-md dark:[--color-muted:var(--color-zinc-900)] ">
+        <div className="p-8 pb-6">
           <div>
             <Link href={"/"}>
-              <h1 className='text-2xl font-bold'>EndPointX</h1>
+              <h1 className="text-2xl font-bold">EndPointX</h1>
             </Link>
-            <h1 className='mb-1 mt-4 text-xl font-semibold'>Sign in to PostMan</h1>
+            <h1 className="mb-1 mt-4 text-xl font-semibold">
+              Sign in to PostMan
+            </h1>
             <p className="text-sm">Welcome back! Sign in to continue</p>
           </div>
 
-          <div className='mt-6 grid grid-cols-1 gap-3'>
-            <Button variant='outline' className='w-full' onClick={() => signIn.social({
-              provider: 'github',
-              callbackURL: "/"
-            })}>
-              <Github className='mr-2 h-4 w-4' />
+          <div className="mt-6 grid grid-cols-1 gap-3">
+            <Button
+              variant="outline"
+              className="w-full cursor-pointer"
+              onClick={() =>
+                signIn.social({
+                  provider: "github",
+                  callbackURL: "/",
+                })
+              }
+            >
+              <Github className="mr-2 h-4 w-4" />
               Sign in with GitHub
             </Button>
           </div>
-          
-          <div className='mt-6 grid grid-cols-1 gap-3'>
-            <Button variant='outline' className='w-full' onClick={() => signIn.social({
-              provider: 'google',
-              callbackURL: "/"
-            })}>
-              <Chrome className='mr-2 h-4 w-4' />
+
+          <div className="mt-6 grid grid-cols-1 gap-3">
+            <Button
+              variant="outline"
+              className="w-full cursor-pointer"
+              onClick={() =>
+                signIn.social({
+                  provider: "google",
+                  callbackURL: "/",
+                })
+              }
+            >
+              <Chrome className="mr-2 h-4 w-4" />
               Sign in with Google
             </Button>
           </div>
